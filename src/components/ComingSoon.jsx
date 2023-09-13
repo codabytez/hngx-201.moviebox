141e6b2fa9df43706b87d9e85fd3fd83c8476769
Obinna Chidi <chidiobinna0001@gmail.com>
2023-09-13 12:46:46 +0100

    worked on coming soon page

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function ComingSoon() {
-  const targetDate = new Date("2023-10-06T00:00:00").getTime();
+  const targetDate = new Date("2023-10-30T00:00:00").getTime();
 
   const [remainingTime, setRemainingTime] = useState(calculateRemainingTime());
 
@@ -33,20 +33,46 @@ function ComingSoon() {
     return () => clearInterval(interval);
   }, []);
 
+  const reloadPage = () => (window.location.href = "/");
+
   return (
-    <div className="min-h-screen min-w-[300px] flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-500">
-      <div className="bg-white p-8 rounded-lg shadow-lg text-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-4">Coming Soon</h1>
-        <p className="text-gray-600 mb-4">
-          We are working hard to bring this page to live.
+    <div className="min-h-screen min-w-[650px] flex items-center justify-center bg-gray-500">
+      <div className="bg-white p-8 rounded-lg shadow-lg text-center flex flex-col gap-2">
+        <h1 className="text-4xl font-bold text-gray-800">Coming Soon</h1>
+        <p className="text-zinc-600 font-medium text-lg">
+          Working on bringing this page to live.
         </p>
-        <div className="text-2xl font-semibold text-gray-800 mb-4">
-          Check back in
+        <div className="text-base text-red-700 font-bold">Check back in</div>
+        <div className="text-lg font-bold text-blue-600 flex justify-evenly gap-3">
+          <div className="bg-[#3A3C42] border shadow-lg w-32">
+            <h4 className="py-2 text-8xl text-white">{remainingTime.days}</h4>
+            <p className="bg-white text-gray-700 w-full">Days</p>
+          </div>
+          <div className="bg-[#3A3C42] border shadow-lg w-32">
+            <h4 className="py-2 text-8xl text-white">{remainingTime.hours}</h4>
+            <p className="bg-white text-gray-700 w-full">Hours</p>
+          </div>
+          <div className="bg-[#3A3C42] border shadow-lg w-32">
+            <h4 className="py-2 text-8xl text-white">
+              {remainingTime.minutes}
+            </h4>
+            <p className="bg-white text-gray-700 w-full">Minutes</p>
+          </div>
+          <div className="bg-[#3A3C42] border shadow-lg w-32">
+            <h4 className="py-2 text-8xl text-white">
+              {remainingTime.seconds > 9
+                ? remainingTime.seconds
+                : `0${remainingTime.seconds}`}
+            </h4>
+            <p className="bg-white text-gray-700 w-full">Seconds</p>
+          </div>
         </div>
-        <div className="text-3xl font-bold text-blue-600">
-          {remainingTime.days}d {remainingTime.hours}h {remainingTime.minutes}m{" "}
-          {remainingTime.seconds}s
-        </div>
+        <button
+          className="mt-4 bg-red-500 text-white py-2 px-8 transition-all rounded-md hover:bg-red-600 focus:outline-none w-max m-auto"
+          onClick={reloadPage}
+        >
+          Go Home
+        </button>
       </div>
     </div>
   );
